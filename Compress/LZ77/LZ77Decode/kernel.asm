b64599bff3ee0149ea5db4544e1751ccb9e971b9
PeterLemon <krom@gbadev.org>
2014-05-08 17:54:43 +0100

    Clean Up

--- Compress/LZ77/LZ77Decode/kernel.asm
+++ Compress/LZ77/LZ77Decode/kernel.asm
@@ -11,7 +11,7 @@ imm32 r0,LZ ; R0 = Source Address
 imm32 r1,Dest ; R1 = Destination Address
 
 ldr r2,[r0],4 ; R2 = Data Length & Header Info
-mov r2,r2,lsr 8 ; R2 = Data Length
+lsr r2,8 ; R2 = Data Length
 add r2,r1 ; R2 = Destination End Offset
 
 LZLoop:
@@ -23,7 +23,7 @@ LZLoop:
     cmp r4,0 ; IF(Flag Data Block Type Shifter == 0) LZLoop
     beq LZLoop
     tst r3,r4 ; Test Block Type
-    mov r4,r4,lsr 1 ; Shift R4 To Next Flag Data Block Type
+    lsr r4,1 ; Shift R4 To Next Flag Data Block Type
     bne LZDecode ; IF(BlockType != 0) LZDecode Bytes
     ldrb r5,[r0],1 ; ELSE Copy Uncompressed Byte
     strb r5,[r1],1 ; Store Uncompressed Byte To Destination
@@ -33,13 +33,13 @@ LZLoop:
 	ldrb r5,[r0],1 ; R5 = Number Of Bytes To Copy & Disp MSB's
 	ldrb r6,[r0],1 ; R6 = Disp LSB's
 	add r6,r5,lsl 8
-	mov r5,r5,lsr 4 ; R5 = Number Of Bytes To Copy (Minus 3)
+	lsr r5,4 ; R5 = Number Of Bytes To Copy (Minus 3)
 	add r5,3 ; R5 = Number Of Bytes To Copy
 	mov r7,$1000
 	sub r7,1 ; R7 = $FFF
 	and r6,r7 ; R6 = Disp
-	add r6,r6,1 ; R6 = Disp + 1
-	sub r6,r1,r6 ; R6 = Destination - Disp - 1
+	add r6,1 ; R6 = Disp + 1
+	rsb r6,r1 ; R6 = Destination - Disp - 1
 	LZCopy:
 	  ldrb r7,[r6],1
 	  strb r7,[r1],1
