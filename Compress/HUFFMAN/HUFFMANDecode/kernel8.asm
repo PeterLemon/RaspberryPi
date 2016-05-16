1819a6b6bf2c06efee89bacc8659c096a6b8a783
PeterLemon <krom@gbadev.org>
2016-05-16 22:04:38 +0100

    R-Pi3 aarch64 kernel8

---
+++ Compress/HUFFMAN/HUFFMANDecode/kernel8.asm
@@ -0,0 +1,81 @@
+; Raspberry Pi 3 'Bare Metal' HUFFMAN Decode Demo by krom (Peter Lemon) & Andy Smith:
+; 1. Decode HUFFMAN Chunks To Memory
+
+code64
+processor cpu64_v8
+format binary as 'img'
+include 'LIB\R_PI2.INC'
+
+org $0000
+
+; Return CPU ID (0..3) Of The CPU Executed On
+mrs x0,MPIDR_EL1 ; X0 = Multiprocessor Affinity Register (MPIDR)
+ands x0,x0,3 ; X0 = CPU ID (Bits 0..1)
+b.ne CoreLoop ; IF (CPU ID != 0) Branch To Infinite Loop (Core ID 1..3)
+
+adr x0,Huff ; X0 = Source Address
+adr x1,Dest ; X1 = Destination Address
+
+ldr w2,[x0],4 ; W2 = Data Length & Header Info
+lsr w2,w2,8 ; W2 = Data Length
+add w2,w2,w1 ; W2 = Destination End Offset
+
+ldrb w3,[x0],1 ; X0 = Tree Table, W3 = (Tree Table Size / 2) - 1
+lsl w3,w3,1
+add w3,w3,1 ; W3 = Tree Table Size
+add w3,w3,w0 ; W3 = Compressed Bitstream Offset
+
+sub x0,x0,5 ; X0 = Source Address
+mov w8,0 ; W8 = Branch/Leaf Flag (0 = Branch 1 = Leaf)
+mov w9,5 ; W9 = Tree Table Offset (Reset)
+HuffChunkLoop:
+  ldr w4,[x3],4 ; W4 = Node Bits (Bit31 = First Bit)
+  mov w5,$80000000 ; W5 = Node Bit Shifter
+
+  HuffByteLoop:
+    cmp w1,w2 ; IF (Destination Address == Destination End Offset) HuffEnd
+    b.eq HuffEnd
+
+    cbz w5,HuffChunkLoop ; IF (Node Bit Shifter == 0) Huff Chunk Loop
+
+    ldrb w6,[x0,x9] ; W6 = Next Node
+    tst w8,1 ; Test W8 == Leaf
+    b.eq HuffChild
+    strb w6,[x1],1 ; Store Data Byte To Destination IF Leaf
+    mov w8,0 ; W8 = Branch
+    mov w9,5 ; W9 = Tree Table Offset (Reset)
+    b HuffByteLoop
+
+    HuffChild:
+      and w7,w6,$3F ; W7 = Offset To Next Child Node
+      lsl w7,w7,1
+      add w7,w7,2 ; W7 = Node0 Child Offset * 2 + 2
+      and w9,w9,$FFFFFFFE ; W9 = Tree Offset NOT 1
+      add w9,w9,w7 ; W9 = Node0 Child Offset
+
+      tst w4,w5 ; Test Node Bit (0 = Node0, 1 = Node1)
+      lsr w5,w5,1 ; Shift R5 To Next Node Bit
+      b.eq HuffNode0
+      add w9,w9,1 ; W9 = Node1 Child Offset
+      mov w10,$40 ; W10 = Test Node1 End Flag
+      b HuffNodeEnd
+      HuffNode0:
+	mov w10,$80 ; W10 = Test Node0 End Flag
+
+      HuffNodeEnd:
+	tst w6,w10 ; Test Node End Flag (1 = Next Child Node Is Data)
+	b.eq HuffByteLoop
+	mov w8,1 ; W8 = Leaf
+	b HuffByteLoop
+  HuffEnd:
+
+Loop:
+  b Loop
+
+CoreLoop: ; Infinite Loop For Core 1..3
+  b CoreLoop
+
+align 4 ; Huffman File Aligned To 4 Bytes
+Huff: file 'RaspiLogo24BPP.huff'
+
+Dest:
